9155e04de04b3107816c8ac5f57ebf9a6fd278f3
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-11-19 15:42:17 -0500

    added to htmlroutes.js to handle the routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,2 @@
+//get request from notes to see the current notes stored in our database
+applicationCache.get(__dirname, "./db.json")
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,4 +2,14 @@
 
 const path = require("path");
 
+//exporting to server.js file.
+module.exports = function(app) {
+    app.get("/notes", function(req, res){
+        res.sendFile(path.join(__dirname, "./notes.html"));
+    });
 
+    //default to home if not visiting from the notes page.
+    app.get("*", function(req, res){
+        res.sendFile(path.join(__dirname, "./index.html"));
+    });
+};
