3470878ee828f5eed65aa80cec8b26a12da4042c
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-11-19 18:00:21 -0500

    fixed fs issue by including fs. fixed db route and htmlroutes file locations.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,13 +1,13 @@
 //get request from notes to see the current notes stored in our database
-
+const fs = require("fs");
 module.exports = function(app) {
     app.get("/api/notes", function(req, res){
-        res.readFile(__dirname,"./db.json", function(err, data) {
+        fs.readFile(__dirname,"../db.json", function(err, data) {
             if (err) throw err;
             const readNotes = JSON.parse(data);
             return res.JSON(readNotes);
         });
     });
 
-    
+
 };
