d2e710a145c76833671c87a248c980ef92089c3b
Daniel Cintron <69943020+DevDan13@users.noreply.github.com>
2020-11-19 16:29:35 -0500

    added get request to display notes to the user.

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,2 +1,13 @@
 //get request from notes to see the current notes stored in our database
-applicationCache.get(__dirname, "./db.json")
+
+module.exports = function(app) {
+    app.get("/api/notes", function(req, res){
+        res.readFile(__dirname,"./db.json", function(err, data) {
+            if (err) throw err;
+            const readNotes = JSON.parse(data);
+            return res.JSON(readNotes);
+        });
+    });
+
+    
+};
